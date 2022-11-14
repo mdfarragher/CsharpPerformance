748b21af29db93eced57d0f1d3201ca5b981b09e
Mark Farragher <mdfarragher@gmail.com>
2022-11-14 14:21:14 +0100

    Added Immutable String example code

---
+++ ImmutableStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Immutable_strings
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string a = "abc";
+			string b = a;
+
+			b += "d";
+
+			// strings are reference types, so this should be true
+            bool c = a.Equals(b);
+
+            // write the result
+            Console.WriteLine($"This should be true: {c}");
+		}
+	}
+}
+
+// We'll need this later
+// Console.WriteLine($"Do a and b point to the same string on the heap? {object.ReferenceEquals(a, b)}");
