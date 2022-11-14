7dd2160760ca20e41505da35d4082ea3db039f77
Mark Farragher <mdfarragher@gmail.com>
2022-11-14 14:08:37 +0100

    Added Boxing and Unboxing example code

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BoxingUnboxing
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int a = 1234;
+			object b = a;
+			int c = (int)b;
+
+			Console.WriteLine (c);
+	
+		}
+	}
+}
